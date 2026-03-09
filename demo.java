270713429924827830a2f55135c20ea5cd5a865e
Atharv Kunde <atharvkunde006@gmail.com>
2026-03-09 21:07:08 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,18 @@
+public class demo {
+    public static void main(String[] Args) {
+        String name = "Atharv";
+        int age = 20;
+        float per = 99.98f;
+        char div = 'c';
+        boolean result = true;
+        boolean result2 = false;
+        System.out.println("My name:" + name);
+        System.out.println("My age is :" + age);
+        System.out.println("My pecenteage is" + per);
+        System.out.println("My div :" + div);
+        System.out.println("My result is:" + result);
+        System.out.println("My age is :" + result2);
+
+    }
+
+}
---
+++ first.java
@@ -0,0 +1,19 @@
+class first {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a = 5;
+        int b = 8;
+        String c = "Atharv";
+        String d = "Kunde";
+        char e = 'a';
+        char f = 'f';
+
+        System.out.println("addition is:" + a + b);
+        System.out.println("multiplication  is:" + a * b);
+        System.out.println("Subtraction  is:" + a / b);
+        System.out.println("addition is:" + a + b);
+        System.out.println("my name is:" + c + " " + d);
+        System.out.println(a + b + c);
+        System.out.println(e + f);
+    }
+}
