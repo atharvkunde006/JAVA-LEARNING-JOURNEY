09a88036b2cb073e7113ce02e1ff72fe55722e31
Atharv Kunde <atharvkunde006@gmail.com>
2026-03-10 23:34:11 +0530

    Add files via upload

---
+++ details.java
@@ -0,0 +1,26 @@
+package atharv;
+
+public class details {
+
+	public static void main(String[] args) {
+		String name="Atharv";
+		char div='c';
+		int rollno=1;
+		String dep="cse";
+		short age= 20;
+		short eng=50;
+		short maths=44;
+		System.out.println("name is:" +name);
+		System.out.println("div is:" + div);
+		System.out.println("Roll is:" +rollno);
+		System.out.println("Department is:" +dep);
+		System.out.println(" Age is:" +age);
+		System.out.println("eng marks is:" +eng);
+		System.out.println(" math marks is:" +maths);
+		System.out.println("reult of math=eng is:"+(eng+maths));
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
