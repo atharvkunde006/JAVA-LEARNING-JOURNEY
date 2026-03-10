308ff373e6a71e714a45450accb1645636831ecd
Atharv Kunde <atharvkunde006@gmail.com>
2026-03-10 23:35:24 +0530

    Add files via upload

---
+++ Myprogram.java
@@ -0,0 +1,25 @@
+package atharv;
+
+public class Myprogram {
+
+	public static void main(String[] args) {
+		String name="Atharv";
+		int age= 20;
+		float per=99.9f;
+		char start='A';
+		boolean pass=true;
+		boolean fail=false;
+		byte no=23;
+		short dis=1;
+		long path=12345788;
+		double hight=999.938774;
+		System.out.println(name+"\n"+age+"\n"+per+"/n"+start+"\n"+pass+"\n"+fail);
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
