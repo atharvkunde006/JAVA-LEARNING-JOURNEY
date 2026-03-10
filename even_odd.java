700125c3a10350d01fc57394e445019e26d56117
Atharv Kunde <atharvkunde006@gmail.com>
2026-03-10 23:34:31 +0530

    Add files via upload

---
+++ even_odd.java
@@ -0,0 +1,10 @@
+package atharv;
+
+public class even_odd {
+	public static void main(String[]args) {
+		int num=70;
+		String result=(num%5==0)&&(num%10==0)?"Divisible":"not divisibl";
+		System.out.println(result);
+	}
+
+}
