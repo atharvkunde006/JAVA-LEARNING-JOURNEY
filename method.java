14a6aaa6c974c90665c233b154cb18ac4aab2278
Atharv Kunde <atharvkunde006@gmail.com>
2026-03-13 22:38:34 +0530

    Add files via upload

---
+++ method.java
@@ -0,0 +1,17 @@
+
+public class method {
+    public static void display() {
+        System.out.println("hii");
+    }
+
+    public static int add(int a, int b) {
+        display();
+        return a + b;
+    }
+
+    public static void main(String[] VisHaL$_9) {
+        System.out.println("Main starts");
+        System.out.println(add(1, 20));
+        System.out.println("Main ends");
+    }
+}
