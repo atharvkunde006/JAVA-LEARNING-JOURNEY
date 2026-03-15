400f2b1291874ae751442b7e2a08d404f67eb987
Atharv Kunde <atharvkunde006@gmail.com>
2026-03-15 21:00:28 +0530

    Add files via upload

---
+++ static_method_1to10.java
@@ -0,0 +1,12 @@
+public class static_method_1to10 {
+
+    static void display() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2" + "x" + i + "=" + i * 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        display();
+    }
+}
