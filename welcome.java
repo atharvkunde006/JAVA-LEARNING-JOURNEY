d62f51aed8ba867532808060e96a132d1b25d242
Atharv Kunde <atharvkunde006@gmail.com>
2026-03-15 20:59:58 +0530

    Add files via upload

---
+++ welcome.java
@@ -0,0 +1,14 @@
+public class welcome {
+    void display()
+
+    {
+        System.out.println("Welcome in java programing");
+    }
+
+    public static void main(String[] args)
+
+    {
+        welcome obj = new welcome();
+        obj.display();
+    }
+}
