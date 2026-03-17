85503c1526cbcbe55a5663273785c5559b1bec64
Atharv Kunde <atharvkunde006@gmail.com>
2026-03-17 23:05:19 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,11 @@
+class loops {
+    loops() {
+    }
+
+    public static void main(String[] var0) {
+        for (int var1 = 1; var1 <= 5; ++var1) {
+            System.out.println("*");
+        }
+        System.out.println("Atharv");
+    }
+}
---
+++ loops2.java
@@ -0,0 +1,10 @@
+package loopsinjava;
+
+public class loops2 {
+    public static void main(String[]args){
+    int i=1;
+    while(i<=25){
+        System.out.println("i");
+    }
+}
+}
