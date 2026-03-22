4cb3c9c903f87914a45646e852bc7425b483a494
Atharv Kunde <atharvkunde006@gmail.com>
2026-03-22 23:42:47 +0530

    Add files via upload

--- loops.java
+++ loops.java
@@ -1,11 +1,11 @@
-class loops {
-    loops() {
-    }
 
-    public static void main(String[] var0) {
-        for (int var1 = 1; var1 <= 5; ++var1) {
-            System.out.println("*");
+public class loops {
+    public static void main(String[] args) throws InterruptedException {
+        int start = 10;
+        for (int i = start; i > 0; i--) {
+            System.out.println(i);
+            Thread.sleep(1000);
         }
-        System.out.println("Atharv");
+        System.out.println("YASH BHAI CHUTIYA ☝️😁😁😁 !");
     }
 }
