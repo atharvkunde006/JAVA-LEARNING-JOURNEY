6e70491a888c3ac84da4debdc6b5d9bc92049a6d
Atharv Kunde <atharvkunde006@gmail.com>
2026-03-31 22:20:53 +0530

    Add files via upload

---
+++ method_formal.java
@@ -0,0 +1,15 @@
+
+public class method_formal {
+    public static void main(String[] args) {
+        int age = 18;
+        String name = "atharv";
+        method(age, name);
+    }
+
+    static void method(int age, String name) {
+        System.out.println("hii");
+        System.out.println("my name is " + name);
+        System.out.println("my age is " + age);
+    }
+
+}
