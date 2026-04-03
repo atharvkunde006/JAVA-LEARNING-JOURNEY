95c87c59a40a61921d6dfc37ecf35fb32ab4bb33
Atharv Kunde <atharvkunde006@gmail.com>
2026-04-03 22:24:27 +0530

    Add files via upload

---
+++ array1.java
@@ -0,0 +1,11 @@
+
+public class array1 {
+    public static void main(String[] args) {
+        String[] cars = { "bmw", "audi", "bugatti", "farrari" };
+        for (int i = 0; i < cars.length; i++) {
+            System.out.print(cars[i] + " ");
+        }
+
+    }
+
+}
